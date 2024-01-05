0430c1fa9876120ce55f28e879764f6ed0953009
Dominik Tornow <dominik.tornow@gmail.com>
2024-01-05 12:22:08 -0800

    Removed all files that are now in .gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/guergabo/github.com/resonatehq/blog/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/guergabo/github.com/resonatehq/blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/guergabo/github.com/resonatehq/blog/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/guergabo/github.com/resonatehq/blog/src/css/custom.css"),
-];
